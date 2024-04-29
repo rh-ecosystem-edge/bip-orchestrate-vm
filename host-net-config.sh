03c61fc052bfed0e4c69e169d2c7717788c06767
Javi Polo <jpolo@redhat.com>
2024-04-29 22:47:45 +0200

    Add apps.CLUSTERNAME.BASE_DOMAIN to dnsmasq configuration

--- host-net-config.sh
+++ host-net-config.sh
@@ -6,6 +6,13 @@ _cleanup(){
 }
 trap _cleanup exit
 
+# _dnsmasq_add_if_not_exists domain_name ip_address
+_dnsmasq_add_if_not_exists(){
+    grep -vE "^address=/$1/" /etc/NetworkManager/dnsmasq.d/bip.conf > $tmpfile
+    echo "address=/$1/$2" >> $tmpfile
+    sudo tee /etc/NetworkManager/dnsmasq.d/bip.conf < $tmpfile
+}
+
 # Update libvirt dhcp configuration
 if sudo virsh net-dumpxml $NET_NAME | grep -q "mac='$HOST_MAC'" ; then
     action=modify
@@ -15,7 +22,6 @@ fi
 sudo virsh net-update $NET_NAME $action ip-dhcp-host '<host mac="'$HOST_MAC'" name="'$HOST_NAME'" ip="'$HOST_IP'"/>' --live --parent-index 0
 
 # Update dnsmasq configuration
-grep -vx address=/api.${CLUSTER_NAME}.${BASE_DOMAIN}/${HOST_IP} /etc/NetworkManager/dnsmasq.d/bip.conf > $tmpfile
-echo address=/api.${CLUSTER_NAME}.${BASE_DOMAIN}/${HOST_IP} >> $tmpfile
-cat $tmpfile | sudo tee /etc/NetworkManager/dnsmasq.d/bip.conf
+_dnsmasq_add_if_not_exists api.${CLUSTER_NAME}.${BASE_DOMAIN} ${HOST_IP}
+_dnsmasq_add_if_not_exists apps.${CLUSTER_NAME}.${BASE_DOMAIN} ${HOST_IP}
 sudo systemctl reload NetworkManager.service
